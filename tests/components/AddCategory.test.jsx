91baf6c9e55d68539cbadbc8b24ed00bbda6016a
Ignacio <jlopez@acl.cl>
2024-03-21 09:53:03 -0300

    test terminado en AddCategory

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -14,10 +14,12 @@ describe('Describir pruebas en AddCategory', () => {
     
   });
 
-  test('debe de llamar a obNewCategory si el input tiene un valor', () => {
+  test('debe de llamar a onNewCategory si el input tiene un valor', () => {
     const inputValue = 'Saitama';
+    //mock de función onNewCategory
+    const onNewcategory = jest.fn();
 
-    render( <AddCategory onNewCategory={ () => {} }/> );
+    render( <AddCategory onNewCategory={ onNewcategory }/> );
     
     //toma las referencias del input y del form
     const input = screen.getByRole('textbox');
@@ -26,9 +28,27 @@ describe('Describir pruebas en AddCategory', () => {
     fireEvent.input( input, { target: { value: inputValue} });
     fireEvent.submit( form );
 
-    expect( input,value ).toBe('');
+    expect( input.value ).toBe('');
+    //se usan los toHave para evalual la fn
+    expect( onNewcategory ).toHaveBeenCalled();
+    expect( onNewcategory ).toHaveBeenCalledTimes(1);
+    expect( onNewcategory ).toHaveBeenCalledWith( inputValue );
 
   })
+
+  test('no debe de llamar onNewCategory si el input esta vacio', () => {
+
+    const onNewcategory = jest.fn();
+
+    render( <AddCategory onNewCategory={ onNewcategory }/> );
+
+    const form = screen.getByRole('form');
+    fireEvent.submit( form );
+
+    expect( onNewcategory ).toHaveBeenCalledTimes(0);
+    
+  })
+  
   
   
 })
